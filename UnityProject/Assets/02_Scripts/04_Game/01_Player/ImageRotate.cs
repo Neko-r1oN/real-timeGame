cf993713f791ba12ee5050405cc74c08168ff0e5
S-JOBI23-054\student <231076@st.yoshida.-g.ac.jp>
2024-11-28 17:53:42 +0900

    プレイヤーのアイドルアニメーション反映

---
+++ UnityProject/Assets/02_Scripts/04_Game/01_Player/ImageRotate.cs
@@ -0,0 +1,32 @@
+////////////////////////////////////////////////////////////////
+///
+///  プレイヤーの画像をカメラから平行に投影する処理
+///  
+///
+////////////////////////////////////////////////////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageRotate : MonoBehaviour
+{
+   
+    GameObject cam;
+    private void Start()
+    {
+        cam = GameObject.Find("MainCamera");
+    }
+    private void Update()
+    {
+        // 対象物と自分自身の座標からベクトルを算出
+        Vector3 vector3 = cam.transform.position - this.transform.position;
+        // もし上下方向の回転はしない(Baseオブジェクトが床から離れないようにする)ようにしたければ以下のようにする。
+        vector3.y = 0f;
+
+        // Quaternion(回転値)を取得
+        Quaternion quaternion = Quaternion.LookRotation(vector3);
+        // 算出した回転値をこのゲームオブジェクトのrotationに代入
+        this.transform.rotation = quaternion;
+    }
+}
